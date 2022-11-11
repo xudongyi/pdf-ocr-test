93bcd03de4b07bf5ec1fd1cdd6a2e503bab2f80e
xu.dongyi <1922448115@qq.com>
2022-11-11 17:25:33 +0800

    1

--- src/main/java/com/example/pdfocrtest/JDoodle.java
+++ src/main/java/com/example/pdfocrtest/JDoodle.java
@@ -13,39 +13,31 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
-/**
- * @ClassName TestPdf
- * @Description
- * @Author xu.dongyi
- * @Date 2022/11/10 14:51
- * @Version 1.0
- **/
-public class TestPdf {
-    static final Tesseract4OcrEngineProperties tesseract4OcrEngineProperties = new Tesseract4OcrEngineProperties();
+
+public class JDoodle {
     private static final String TESS_DATA_FOLDER = "D:\\Program Files\\Tesseract-OCR\\tessdata";
-    private static List LIST_IMAGES_OCR = Arrays.asList(new File("C:\\Users\\xu.dongyi\\Desktop\\emobile_2022-11-10_14-42-26.png"));
-    private static String OUTPUT_PDF = "emobile_2022-11-10_14-42-26.pdf";
+
+	static final Tesseract4OcrEngineProperties tesseract4OcrEngineProperties = new Tesseract4OcrEngineProperties();
+    private static List LIST_IMAGES_OCR = Arrays.asList(new File("C:\\Users\\xu.dongyi\\Desktop\\emobile_2022-11-11_15-56-11.png"));
+    private static String OUTPUT_PDF = "emobile_2022-11-11_15-56-11.pdf";
 
     public static void main(String[] args) throws IOException {
-
         final Tesseract4LibOcrEngine tesseractReader = new Tesseract4LibOcrEngine(tesseract4OcrEngineProperties);
+        tesseract4OcrEngineProperties.setPathToTessData(new File(TESS_DATA_FOLDER));
         tesseract4OcrEngineProperties.setPathToTessData(new File(TESS_DATA_FOLDER));
         tesseract4OcrEngineProperties.setLanguages(Arrays.asList("chi_sim"));
         OcrPdfCreatorProperties properties = new OcrPdfCreatorProperties();
         properties.setPdfLang("zh");
-        properties.setTextColor(DeviceRgb.RED);
+//        properties.setTextColor(DeviceRgb.RED);
         properties.setTextLayerName("text");
         properties.setImageLayerName("image");
-
+        OcrPdfCreator ocrPdfCreator = new OcrPdfCreator(tesseractReader, properties);
         final PdfOcrFontProvider fontProvider = new PdfOcrFontProvider();
         fontProvider.addFont("C:\\Windows\\Fonts\\simhei.ttf");
         properties.setFontProvider(fontProvider);
-
-        OcrPdfCreator ocrPdfCreator = new OcrPdfCreator(tesseractReader, properties);
-
-
         try (PdfWriter writer = new PdfWriter(OUTPUT_PDF)) {
             ocrPdfCreator.createPdf(LIST_IMAGES_OCR, writer).close();
         }
     }
+
 }
